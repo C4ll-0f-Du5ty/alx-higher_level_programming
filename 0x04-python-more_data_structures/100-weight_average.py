8b9f78591082c4d64b8eb65d1ac0ec0c3ef01f7a
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-12-06 21:08:57 +0200

    done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if not my_list:
         return 0
     s1 = sum(i[1] for i in my_list)
     s2 = sum(map(lambda x: x[0] * x[1], my_list))

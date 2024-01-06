420333ef837b30ff9c020839bb4953f996deb03a
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-06 18:07:46 +0200

    done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""This is a module that demonstrates the use of __slots__."""
+
+
 class LockedClass:
+    """
+    Attributes:
+    - allowed_attribute: An attribute that users can assign values to.
+    'Hello'
+    """
+
     __slots__ = ['first_name']

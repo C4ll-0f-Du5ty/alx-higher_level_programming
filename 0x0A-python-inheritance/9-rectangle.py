e3c2a4da7ceb4e6ef1b5f1aaf0be45946e1511d6
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-08 22:35:37 +0200

    done

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""inheritance example module
+"""
+
+
+module = __import__('7-base_geometry')
+BaseGeometry = getattr(module, 'BaseGeometry')
+
+
+class Rectangle(BaseGeometry):
+    """new class to set some
+    attributes
+"""
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+       return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"

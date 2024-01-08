ee2289f462172532e1e9a8b05e8ca6b0f637bd4c
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-08 18:16:38 +0200

    done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""checking the type of an
+    object
+"""
+
+
+def inherits_from(obj, a_class):
+    """return true or false depending
+        on the provided object if it an
+        instnace of the provided class
+    """
+    return not (type(obj) is a_class)

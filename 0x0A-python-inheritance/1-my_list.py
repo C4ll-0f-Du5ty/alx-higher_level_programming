2c2c2141a2b4e0c201d219409afc9657dfd62c8b
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-08 16:58:44 +0200

    done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""sorting list module
-    """
+"""sorting list module and printing 
+    it
+"""
 
 
 class MyList(list):
     """Function to return a sorted list
+    and then printing it
     """
     def __init__(self):
         super().__init__()

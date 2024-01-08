adcd3ef7e4845bc6a3d035720ba9ae54fd00ff95
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-08 22:42:52 +0200

    done

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""inheritance example module
+"""
+
+
+module = __import__('9-rectangle')
+rectangle = getattr(module, 'Rectangle')
+
+
+class Square(rectangle):
+    """new class to set some
+    attributes
+"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size

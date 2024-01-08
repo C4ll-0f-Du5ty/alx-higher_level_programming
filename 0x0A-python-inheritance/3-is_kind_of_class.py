d05181f869ee487a3039421c3a7d10326bdec739
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-08 18:08:51 +0200

    done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""checking the type of an
+    object
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return true or false depending
+    on the provided object if it an
+    instnace of the provided class
+"""
+    return isinstance(obj, a_class)

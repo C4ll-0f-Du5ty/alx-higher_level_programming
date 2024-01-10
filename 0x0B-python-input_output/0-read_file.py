890a808d0a30a068b837290d614e74c898c02e7d
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-10 18:18:33 +0200

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Starting with files"""
+
+
 def read_file(filename=""):
+    """Reading from a given file"""
     with open(filename, encoding='UTF_8') as file:
         print(file.read(), end="")
 

619a432268843074556c21bf98274abfb8f4b357
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-10 21:23:12 +0200

    done

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,8 +2,6 @@
 """returns the dictionary description with simple data structure"""
 
 
-import json
-
 def class_to_json(obj):
     """returns the dictionary description with simple data structure"""
-    return json.dumps(obj.__dict__)
+    return str(obj.__dict__)

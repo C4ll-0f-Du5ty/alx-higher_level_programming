cf3b0930d3ea126f16589bd75867450accd5f202
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-16 18:54:53 +0200

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,16 @@ class Rectangle(Base):
         """a method to be called when we print our instance!"""
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y}"
                 f" - {self.width}/{self.height}")
+
+    def update(self, *args):
+        """updating my class attributes!"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]

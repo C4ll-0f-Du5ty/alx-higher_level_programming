322b36ca9b3603210db4cdb9921db5319c6d3f77
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-12 01:09:12 +0200

    done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const g = process.argv.slice(2)
-if (g.length == 1)
-    console.log('Argument found')
-else if (g.length > 1)
-    console.log('Arguments found')
-else
-    console.log('No argument')
+const g = process.argv.slice(2);
+if (g.length === 1) { console.log('Argument found'); }
+else if (g.length > 1) { console.log('Arguments found'); }
+else { console.log('No argument'); }

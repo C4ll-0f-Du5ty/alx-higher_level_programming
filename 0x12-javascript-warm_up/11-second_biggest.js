3085a5b536ab51455848935a18535f281e06d71b
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-12 22:32:51 +0200

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (g.length === 0 || g.length === 1) {
   console.log(0);
 } else {
   const m = Math.max(...g);
-  let k = 0;
+  let k = g[0];
   for (let i = 0; i < g.length; i++) {
     if (g[i] < m && g[i] > k) {
       k = g[i];

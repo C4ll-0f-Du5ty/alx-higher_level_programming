d81b5b54538b74b814517002a593349987bd9dc5
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-12 21:39:35 +0200

    done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const g = process.argv.slice(2);
+if (g.length === 0 || isNaN(parseInt(g[0]))) {
+  console.log('Missing size');
+}
+else {
+  for (let i = 0; i < g[0]; i++) {
+    console.log('x'.repeat(g[0]));
+  }
+}

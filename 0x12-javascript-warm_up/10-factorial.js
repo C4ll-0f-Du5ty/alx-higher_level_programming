64a1c1bc5b4d5c72c3ded949d1fb4b96e6a065d2
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-12 23:10:45 +0200

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-function factorial(x) {
+function factorial (x) {
   if (x === 0 || isNaN(x)) {
     return 1;
   }
-  return x * factorial(x - 1)
+  return x * factorial(x - 1);
 }
 
 const g = process.argv.slice(2);
 console.log(factorial(parseInt(g[0])));
-

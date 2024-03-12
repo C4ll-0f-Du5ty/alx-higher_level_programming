918815d9b7b1c0cdd9112ddd6348161c1de1a3de
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-12 22:24:21 +0200

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
-function factorial(x, y) {
+function factorial (x, y) {
   if (y > 1) {
     return factorial(x * (y - 1), y - 1);
-  }
-  else {
+  } else {
     return x;
   }
-
 }
 
-const g = process.argv.slice(2)
+const g = process.argv.slice(2);
 if (g[0] === undefined) {
-  console.log(1)
+  console.log(1);
 } else {
-  console.log(factorial(parseInt(g[0]), parseInt(g[0])))
+  console.log(factorial(parseInt(g[0]), parseInt(g[0])));
 }

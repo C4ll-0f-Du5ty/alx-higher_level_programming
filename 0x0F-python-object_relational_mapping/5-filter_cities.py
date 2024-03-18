8ef71e04c6b15eb29c88330379876f2479964bab
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-18 21:44:11 +0200

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     cursor.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')\
                     FROM cities INNER JOIN states ON \
                     states.id = cities.state_id \
-                    WHERE states.name = %s", (sys.argv[4], ))
+                    WHERE states.name = %s \
+                    ORDER BY cities.id ASC", (sys.argv[4], ))
 
     rows = cursor.fetchall()
     for row in rows:

d5336c91c713b09f0f870f600449c04ff1a21f66
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-27 17:51:23 +0200

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # printing a specific feild of the response of http
+# -I for fetching the HTTP-Header only
 curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# sending a delete Request
+# -X for Method
+curl -sX DELETE "$1"

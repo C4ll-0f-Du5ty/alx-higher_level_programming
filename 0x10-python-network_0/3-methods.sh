5799de686956a7c829d6f7f37b94ec8e69ed1174
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-27 21:14:46 +0200

    done

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Showing allowed methods
-curl -sI "$1" | grep -i "Allow:" | awk '{for(i = 2; i <= NF; i++) printf (i == 2 ? "" : " ") $i}'
+curl -sI "$1" | grep -i "Allow:" | awk '{for(i = 2; i <= NF; i++) printf (i == 2 ? "" : " ") $i; print""}'

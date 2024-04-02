ada23657c7afd4f624c3b742b4d522621d2c898f
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-04-02 03:44:12 +0200

    done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""Getting My Id from my GitHub"""
 import sys
 import requests
+
 
 def get_github_user_id(username, token):
     url = "https://api.github.com/user"
     headers = {"Authorization": f"token {token}"}
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         return response.json().get('id')
     else:
         return None
+
 
 if __name__ == "__main__":
     username = sys.argv[1]

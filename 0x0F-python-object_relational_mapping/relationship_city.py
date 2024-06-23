205bcd0bcc315d7b53bdaf3bce4c9ad7460a1d5a
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-06-23 20:18:33 +0300

    d

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -8,7 +8,7 @@ from relationship_state import Base, State
 class City(Base):
     """My City Table In DB"""
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     # state = relationship("State", back_populates="cities")
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -11,7 +11,7 @@ Base = declarative_base(MetaData=mymetadata)
 class State(Base):
     """My State Table"""
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    id = Column(Integer, nullable=False, primary_key=True, unique=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="states")
     # Relationship with City

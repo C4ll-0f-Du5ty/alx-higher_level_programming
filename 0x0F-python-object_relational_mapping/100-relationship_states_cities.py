31c94a1287404a17478b106653d4a28fe9885dff
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-06-23 20:06:56 +0300

    d

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
     # Create the tables if they don't exist
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     # Add a new state
     california = State(name="California")

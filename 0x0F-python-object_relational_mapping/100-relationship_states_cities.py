357af58cad25d3c1c9d3910e7d2e4a4c1b40a339
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-06-23 19:21:09 +0300

    d

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -26,7 +26,7 @@ def main(username, password, dbname):
         session.add(san_francisco)
         session.commit()
 
-        print("State and City added successfully.")
+        # print("State and City added successfully.")
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:

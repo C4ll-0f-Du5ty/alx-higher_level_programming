985ff016c22455d140966264611cdb6b121c653b
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-06-25 18:21:21 +0300

    done

--- 0x0F-python-object_relational_mapping/temp.py
+++ 0x0F-python-object_relational_mapping/temp.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
                           user=sys.argv[1],
                           passwd=sys.argv[2],
                           db=sys.argv[3])
-    command = "SELECT * FROM states WHERE name = %s order by id ASC"
+    command = "SELECT cities.name from cities inner join states where states.id = cities.state_id and states.name = %s order by cities.id ASC"
     cursor = con.cursor()
-    cursor.execute(command, (sys.argv[4], ))
+    cursor.execute(command, (sys.argv[4],))
     rows = cursor.fetchall()
     for r in rows:
         print(r)

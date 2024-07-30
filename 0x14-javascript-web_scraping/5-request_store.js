81011002fd75d6c1e7f5c1a934ac0a06700cea34
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-30 20:56:52 +0300

    done

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 const request = require('request');
 const args = process.argv;
 request(args[2], (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  fs.writeFile(args[3], response.body, 'utf8', error => {
     if (error) {
-        console.error(error);
-        return;
+      console.error(error);
     }
-    fs.writeFile(args[3], response.body, 'utf8', error => {
-        if (error) {
-            console.error(error)
-            return;
-        }
-    })
+  });
 }
 );

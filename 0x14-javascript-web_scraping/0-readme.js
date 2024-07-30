fce1857c825cce90051f3d72eb40c47c1d593de2
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-30 19:04:09 +0300

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv;
 
+fs.readFile(args[2], 'utf8', (error, Data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(Data);
+}
+);

d9c254fcdb63ef6f2757b816ec0f55a8a068fd2c
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-30 20:22:44 +0300

    done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/people/18', (error, response, body) => {
+const args = process.argv;
+let count = 0;
+request(args[2], (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  console.log(JSON.parse(body).films.length);
+  for (const s of JSON.parse(body).results) {
+    for (const m of s.characters) {
+      if (m.endsWith('/18/')) { count++; }
+    }
+  }
+  console.log(count);
 }
 );

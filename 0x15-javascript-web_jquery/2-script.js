636f5db40234184a5d03ef19953aaf991fbff76b
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-08-10 19:23:08 +0300

    done

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const header = document.querySelector("DIV#red_header")
-header.addEventListener("click", function () {
-    this.style.color = "#FF0000"
+$(document).ready(function () {
+    $("DIV#red_header").click(function () {
+        $("header").css("color", "#FF0000")
+    })
 })
